56f1ced696cfa1775e38c3564f08b736d6cb3fb7
sness <snessnet@gmail.com>
2023-12-04 12:49:55 -0800

    more

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,19 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+import requests
 
+# Set up the title for your app
 st.title('test123')
 
+# The URL of the API endpoint
+url = 'http://34.70.221.101/conversations/1'
 
+# Make the GET request to the API
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # If successful, print the response
+    st.write(response.text)
+else:
+    # If unsuccessful, print the error code
+    st.write(f'Failed to retrieve data: Status code {response.status_code}')
